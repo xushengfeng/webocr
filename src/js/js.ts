2b444d70e3580fe4d728b5b0533319c54a13b910
xushengfeng <xushengfeng_zg@163.com>
2025-04-17 20:47:18 +0800

    主要 更新依赖

--- src/js/js.ts
+++ src/js/js.ts
@@ -1,9 +1,9 @@
 import "../../css/css.css";
 
 if ("serviceWorker" in navigator) {
-    if (import.meta.env.PROD) {
-        navigator.serviceWorker.register("/sw.js");
-    }
+	if (import.meta.env.PROD) {
+		navigator.serviceWorker.register("/sw.js");
+	}
 }
 
 const drop_el = document.getElementById("drop");
@@ -14,116 +14,118 @@ const output_el = document.getElementById("r") as HTMLTextAreaElement;
 const run_el = document.getElementById("run");
 
 drop_el.ondragover = (e) => {
-    e.preventDefault();
+	e.preventDefault();
 };
 drop_el.ondrop = (e) => {
-    e.preventDefault();
-    put_datatransfer(e.dataTransfer);
+	e.preventDefault();
+	put_datatransfer(e.dataTransfer);
 };
 drop_el.onpaste = (e) => {
-    e.preventDefault();
-    put_datatransfer(e.clipboardData);
+	e.preventDefault();
+	put_datatransfer(e.clipboardData);
 };
 upload_pel.onclick = () => {
-    upload_el.click();
+	upload_el.click();
 };
 upload_el.onchange = () => {
-    let files = upload_el.files;
-    for (let f of files) {
-        let type = f.type.split("/")[0];
-        if (type != "image") continue;
-        let reader = new FileReader();
-        reader.readAsDataURL(f);
-        reader.onload = () => {
-            let el = create_img(reader.result as string);
-            imgs_el.append(el);
-        };
-    }
+	let files = upload_el.files;
+	for (let f of files) {
+		let type = f.type.split("/")[0];
+		if (type != "image") continue;
+		let reader = new FileReader();
+		reader.readAsDataURL(f);
+		reader.onload = () => {
+			let el = create_img(reader.result as string);
+			imgs_el.append(el);
+		};
+	}
 };
 run_el.classList.add("no_run");
 run_el.onclick = () => {
-    run_ocr();
+	run_ocr();
 };
 document.getElementById("close").onclick = () => {
-    output_el.value = "";
-    output = [];
-    imgs_el.innerHTML = "";
+	output_el.value = "";
+	output = [];
+	imgs_el.innerHTML = "";
 };
 
 var ocr_init = false;
 
 /** 拖放数据处理 */
 function put_datatransfer(data: DataTransfer) {
-    if (data.files.length != 0) {
-        for (let f of data.files) {
-            let type = f.type.split("/")[0];
-            if (type != "image") continue;
-            let reader = new FileReader();
-            reader.readAsDataURL(f);
-            reader.onload = () => {
-                let el = create_img(reader.result as string);
-                imgs_el.append(el);
-            };
-        }
-    } else {
-    }
+	if (data.files.length != 0) {
+		for (let f of data.files) {
+			let type = f.type.split("/")[0];
+			if (type != "image") continue;
+			let reader = new FileReader();
+			reader.readAsDataURL(f);
+			reader.onload = () => {
+				let el = create_img(reader.result as string);
+				imgs_el.append(el);
+			};
+		}
+	} else {
+	}
 }
 
 function create_img(src: string) {
-    let div = document.createElement("div");
-    div.classList.add("img_el");
-    let image = document.createElement("img");
-    image.src = src;
-    image.onload = () => {
-        image.setAttribute("data-w", String(image.width));
-        image.setAttribute("data-h", String(image.height));
-    };
-    div.append(image);
-    return div;
+	let div = document.createElement("div");
+	div.classList.add("img_el");
+	let image = document.createElement("img");
+	image.src = src;
+	image.onload = () => {
+		image.setAttribute("data-w", String(image.width));
+		image.setAttribute("data-h", String(image.height));
+	};
+	div.append(image);
+	return div;
 }
 
 function run_ocr() {
-    if (!ocr_init) return;
-    output_el.value = "";
-    output = [];
-    imgs_el.querySelectorAll(":scope > div > div").forEach((el: HTMLElement) => {
-        el.innerHTML = "";
-    });
-    imgs_el.querySelectorAll(":scope > div > img").forEach((el: HTMLImageElement, i) => {
-        to_text(el, i);
-    });
+	if (!ocr_init) return;
+	output_el.value = "";
+	output = [];
+	imgs_el.querySelectorAll(":scope > div > div").forEach((el: HTMLElement) => {
+		el.innerHTML = "";
+	});
+	imgs_el
+		.querySelectorAll(":scope > div > img")
+		.forEach((el: HTMLImageElement, i) => {
+			to_text(el, i);
+		});
 }
 
 function to_text(img: HTMLImageElement, i: number) {
-    let canvas = document.createElement("canvas");
-    let w = img.naturalWidth,
-        h = img.naturalHeight;
-    canvas.width = w;
-    canvas.height = h;
-    canvas.getContext("2d").drawImage(img, 0, 0);
-    ocr.ocr(canvas.getContext("2d").getImageData(0, 0, w, h)).then((v) => {
-        let tl = [];
-        let div = document.createElement("div");
-        for (let i of v) {
-            if (!i.text) continue;
-            tl.push(i.text);
-            let x0 = i.box[0][0];
-            let y0 = i.box[0][1];
-            let x1 = i.box[2][0];
-            let y1 = i.box[2][1];
-            let xel = document.createElement("p");
-            xel.style.left = `${(x0 / w) * 100}%`;
-            xel.style.top = `${(y0 / h) * 100}%`;
-            xel.style.width = `${((x1 - x0) / w) * 100}%`;
-            xel.style.height = `${((y1 - y0) / h) * 100}%`;
-            div.append(xel);
-            xel.innerText = i.text;
-        }
-        img.parentElement.append(div);
-        console.log(tl);
-        output[i] = tl.join("\n");
-        output_el.value = output.join("\n");
-    });
+	let canvas = document.createElement("canvas");
+	let w = img.naturalWidth,
+		h = img.naturalHeight;
+	canvas.width = w;
+	canvas.height = h;
+	canvas.getContext("2d").drawImage(img, 0, 0);
+	ocr.ocr(canvas.getContext("2d").getImageData(0, 0, w, h)).then((v) => {
+		let tl = [];
+		let div = document.createElement("div");
+		for (let i of v.parragraphs) {
+			if (!i.text) continue;
+			tl.push(i.text);
+			let x0 = i.box[0][0];
+			let y0 = i.box[0][1];
+			let x1 = i.box[2][0];
+			let y1 = i.box[2][1];
+			let xel = document.createElement("p");
+			xel.style.left = `${(x0 / w) * 100}%`;
+			xel.style.top = `${(y0 / h) * 100}%`;
+			xel.style.width = `${((x1 - x0) / w) * 100}%`;
+			xel.style.height = `${((y1 - y0) / h) * 100}%`;
+			div.append(xel);
+			xel.innerText = i.text;
+		}
+		img.parentElement.append(div);
+		console.log(tl);
+		output[i] = tl.join("\n");
+		output_el.value = output.join("\n");
+	});
 }
 
 import * as ocr from "esearch-ocr";
@@ -133,17 +135,15 @@ start();
 import dic from "../../public/ocr/ppocr_keys_v1.txt?raw";
 
 async function start() {
-    await ocr.init({
-        detPath: "./ocr/ppocr_det.onnx",
-        recPath: "./ocr/ppocr_rec.onnx",
-        dic: dic,
-        dev: false,
-        ort: window["ort"],
-        detShape: [640, 640],
-        cv: window["cv"],
-    });
-    ocr_init = true;
-    run_el.classList.remove("no_run");
+	await ocr.init({
+		detPath: "./ocr/ppocr_det.onnx",
+		recPath: "./ocr/ppocr_rec.onnx",
+		dic: dic,
+		dev: false,
+		ort: window["ort"],
+	});
+	ocr_init = true;
+	run_el.classList.remove("no_run");
 }
 
 let output = [];
